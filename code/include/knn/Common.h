6a048a7dec6dd766107149e3633d5518cdad77de
Johannes de Fine Licht <johannes.definelicht@cern.ch>
2015-11-10 18:17:05 +0100

    log2 implementation for integers.

--- code/include/knn/Common.h
+++ code/include/knn/Common.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdint.h>
 #include <vector>
 
 namespace knn {
@@ -10,4 +11,11 @@ using DataItr = typename std::vector<T>::const_iterator;
 template <typename T>
 using DataContainer = std::vector<T>;
 
+template <typename T> int log2(T x) {
+  int highestBit = 0;
+  while (x >>= 1) {
+    ++highestBit;
+  }
+  return highestBit;
+}
 }
